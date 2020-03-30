a5ab99708ff3a190bd5b4b09ba7067029ad24ca4
Crowdbotics <team@crowdbotics.com>
2020-03-30 09:14:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/guitar_simplified_15227/MainActivity.java
@@ -0,0 +1,15 @@
+package com.guitar_simplified_15227;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "guitar_simplified_15227";
+    }
+}
